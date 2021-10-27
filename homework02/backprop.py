d57a55fabd56393e42f52cad004a92665af0e48f
jalippold <jlippold@uos.de>
2021-10-27 22:40:02 +0200

    Added high level MLP calculate function

--- homework02/backprop.py
+++ homework02/backprop.py
@@ -38,6 +38,7 @@ class MLP():
     def __init__(self, input_units, layers, output_units):
         """layers is an array, containing the input and hidden layers
             It's length-1 is the number of hidden layers, the elements are the number of neurons on every layer"""
+        self.input_units = input_units
         self.input_layer = []
         self.hidden_layers = []
         self.output_neurons = []
@@ -57,7 +58,39 @@ class MLP():
             self.output_neurons.append(Perceptron(layers[len(layers)-1]))
 
 
+    def calculate(self, input):
+        if len(input) is not self.input_units:
+            raise ValueError("Len of input does not match networks structure!")
 
+        vals = []
+        next_vals = []
+
+        for i in len(self.input_layer):
+            next_vals.append(self.input_layer[i].forward_step(input))
+        
+
+        for i in range(len(self.hidden_layers)):
+            vals = next_vals.copy()
+
+            if len(vals) is not len(self.hidden_layers[i]):
+                raise IndexError("Somethings wrong in the internal structure!")
+
+            next_vals.clear()
+            for j in range(self.hidden_layers[i]):
+                next_vals.append(self.hidden_layers[i][j].forward_step(vals))
+
+        if len(next_vals) is not len(self.output_neurons):
+            raise IndexError("Somethings wrong in the internal structure!")
+
+        outputs = []
+        for i in range(len(self.output_neurons)):
+            outputs.append(self.output_neurons[i].forward_step(next_vals))
+
+        return outputs
+
+    def backpropagate(self, output, expected_vals):
+        # Use loss function and backpropagation to change Perceptrons weights
+        pass
 
 # MLP with 2 inputs, an input layer with 4 perceptrons, one hidden layer with 4 perceptrons and an output layer with a single perceptron
 mlp = MLP(2, [4, 4], 1)
