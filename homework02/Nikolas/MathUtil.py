c9853396037bc6404f96624018b9ead3adf621a5
Nikolas Wintering <nwintering@uni-osnabrueck.de>
2021-10-31 13:54:24 +0100

    Tried fixing issue by making integers to explicit floats.

--- homework02/Nikolas/MathUtil.py
+++ homework02/Nikolas/MathUtil.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return 1.0 / (1.0 + np.exp(-x))
 
 def sigmoidprime(x):
     sig = sigmoid(x)
-    return sig*(1-sig)
+    return sig*(1.0-sig)
