60b3a31ae328bbb5a48d30d36a4e34cc040e995b
Nikolas Wintering <nwintering@uni-osnabrueck.de>
2021-11-04 17:19:22 +0100

    Refactored project and documented everything.

--- homework02/Perceptron.py
+++ homework02/Perceptron.py
@@ -3,7 +3,10 @@ from MathUtil import *
 
 class Perceptron():
     def __init__(self, input_units):
-        """Perceptron with bias and i ingoing weighted activations"""
+        """
+        Perceptron with bias, weights and ingoing activations.
+        :param input_units is the number of inputs going into this perceptron
+        """
         self.bias = np.random.randn()
         self.weights = np.array(np.random.randn(input_units))
         self.alpha = 1.0    # according to homework sheet this should be 1.0
@@ -11,6 +14,10 @@ class Perceptron():
         self.inputs = None
 
     def forward_step(self, inputs):
+        """
+        Calculates the output of this perceptron.
+        :param inputs are the activations going into this perceptron
+        """
         if len(inputs) is not len(self.weights):
             raise IndexError(f"Length of input ({len(inputs)}) does not match length of weights ({len(self.weights)})!")
         # remember input/activations for backpropagation
@@ -20,7 +27,10 @@ class Perceptron():
         
 
     def update(self, delta):
-        # update all weights with the one delta multiplied by the matching input/activation multiplied with the learning rate (alpha)
+        """
+        Update all weights and bias with the one delta multiplied by the matching input/activation multiplied with the learning rate (alpha).
+        :param delta is the value of the delta-rule
+        """
         # notation on the homework sheet is not that clear but I think updates are like this:
         self.weights -= self.alpha * delta * self.inputs
         self.bias -= self.alpha * delta * 1.0
