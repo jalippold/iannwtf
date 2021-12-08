1e53f4900a01bf99a7a45e7b79acef3c8cc08b9e
jalippold <jlippold@uos.de>
2021-12-08 11:29:57 +0100

    Task 1 done

---
+++ homework07/mainlstm.py
@@ -0,0 +1,163 @@
+import tensorflow as tf
+from tensorflow.python.framework.tensor_spec import TensorSpec
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import argparse
+
+SEQ_LEN = 3
+NUM_SAMPLES = 3
+
+MIN_VAL = -1
+MAX_VAL = 1
+
+def integration_task(seq_len, num_samples):
+    for i in range(num_samples):
+        input = tf.random.uniform(shape=(seq_len,), minval=MIN_VAL, maxval=MAX_VAL, dtype=tf.float32)
+        target = 1 if tf.math.reduce_sum(input, axis=-1) > 0 else 0
+        yield (input, tf.constant(target, dtype=tf.float32))
+
+
+def my_integration_task():
+    for data in integration_task(SEQ_LEN, NUM_SAMPLES):
+        yield data
+
+
+ds = tf.data.Dataset.from_generator(my_integration_task, 
+            output_signature=(tf.TensorSpec(shape=(SEQ_LEN,), dtype=tf.float32), tf.TensorSpec(shape=(), dtype=tf.float32)))
+
+
+################################
+# Building Dataset done!
+################################
+
+
+
+def prepare_myds(myds):
+    # we don't have to do that much, we build the dataset like we want it...
+
+    # cache this progress in memory, as there is no need to redo it; it is deterministic after all
+    myds = myds.cache()
+    # shuffle, batch, prefetch
+    myds = myds.shuffle(1000)
+    myds = myds.batch(64)
+    myds = myds.prefetch(32)
+    # return preprocessed dataset
+    return myds
+
+
+@tf.function
+def train_step(model, input, target, loss_function, optimizer):
+    """
+    This function executes a training step on the given network.
+    Using gradient descend
+    :param model: the network model
+    :param input: the given input tensors
+    :param target: the given target tensors
+    :param loss_function: the given loss function
+    :param optimizer: the given optimizer
+    :return: the loss during this trainin step
+    """
+    # loss_object and optimizer_object are instances of respective tensorflow classes
+    with tf.GradientTape() as tape:
+        prediction = model(input, training=True)
+        loss = loss_function(target, prediction)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    return loss
+
+def test(model, test_data, loss_function):
+    """
+    # Tests over complete test data
+    :param model: the network model
+    :param test_data: the given input tensors
+    :param loss_function: the given loss function
+    :return: the test loss and test accuracy
+    """
+    test_accuracy_aggregator = []
+    test_loss_aggregator = []
+    for (input, target) in test_data:
+        prediction = model(input, training=False)
+        sample_test_loss = loss_function(target, prediction)
+        sample_test_accuracy =  np.argmax(target, axis=1) == np.argmax(prediction, axis=1)
+        sample_test_accuracy = np.mean(sample_test_accuracy)
+        test_accuracy_aggregator.append(sample_test_accuracy)
+        test_loss_aggregator.append(sample_test_loss.numpy())
+
+    test_loss = tf.reduce_mean(test_loss_aggregator)
+    test_accuracy = tf.reduce_mean(test_accuracy_aggregator)
+    return test_loss, test_accuracy
+
+
+
+if __name__ == "__main__":
+    #parse arguments to determine which model should be used
+    parser = argparse.ArgumentParser(description='ResNet or DenseNet')
+    parser.add_argument('--model', type=str, help='ResNet of DenseNet', required=True)
+    args = parser.parse_args()
+
+    tf.keras.backend.clear_session()
+    
+    # loading the data set
+    train_ds = tf.data.Dataset.from_generator(my_integration_task, 
+            output_signature=(tf.TensorSpec(shape=(SEQ_LEN,), dtype=tf.float32), tf.TensorSpec(shape=(), dtype=tf.float32)))
+    test_ds = tf.data.Dataset.from_generator(my_integration_task, 
+            output_signature=(tf.TensorSpec(shape=(SEQ_LEN,), dtype=tf.float32), tf.TensorSpec(shape=(), dtype=tf.float32)))
+    
+    train_ds = train_ds.apply(prepare_myds)
+    test_ds = test_ds.apply(prepare_myds)
+    #print(train_ds.take(1))
+    # test preprocessing
+    #for elem in train_ds:
+    #    print(elem)
+    #    break
+    ### Hyperparameters
+    num_epochs = 30
+    learning_rate = tf.constant(0.001, dtype=tf.float32)
+    # Initialize the loss: categorical cross entropy.
+    cross_entropy_loss = tf.keras.losses.CategoricalCrossentropy()
+    # Initialize the optimizer: SGD with default parameters.
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    # Initialize lists for later visualization.
+    train_losses = []
+    test_losses = []
+    test_accuracies = []
+
+    #create the model: TODO!
+    model = None
+
+    # testing once before we begin
+    test_loss, test_accuracy = test(model, test_ds, cross_entropy_loss)
+    test_losses.append(test_loss)
+    test_accuracies.append(test_accuracy)
+    # check how model performs on train data once before we begin
+    train_loss, _ = test(model, train_ds, cross_entropy_loss)
+    train_losses.append(train_loss)
+    #   We train for num_epochs epochs.
+    for epoch in range(num_epochs):
+        start_time = datetime.datetime.now()
+        print(f'Epoch: {str(epoch)} starting with accuracy {test_accuracies[-1]}')
+        # training (and checking in with training)
+        epoch_loss_agg = []
+        for input, target in train_ds:
+            train_loss = train_step(model, input, target, cross_entropy_loss, optimizer)
+            epoch_loss_agg.append(train_loss)
+        # track training loss
+        train_losses.append(tf.reduce_mean(epoch_loss_agg))
+        # testing, so we can track accuracy and test loss
+        test_loss, test_accuracy = test(model, test_ds, cross_entropy_loss)
+        test_losses.append(test_loss)
+        test_accuracies.append(test_accuracy)
+        diff_time = datetime.datetime.now() - start_time
+        print(f"Epoch {epoch} took {diff_time} to complete.")
+
+    print(model.summary())
+    # Visualize accuracy and loss for training and test data.
+    plt.subplot(1, 1, 1)
+    line1, = plt.plot(train_losses, '-x')
+    line2, = plt.plot(test_losses, '-+')
+    line3, = plt.plot(test_accuracies, '-o')
+    plt.xlabel("Training steps")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend((line1, line2, line3), ("training loss", "test loss", "test accuracy"))
+    plt.show()
